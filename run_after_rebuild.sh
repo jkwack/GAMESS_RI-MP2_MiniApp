0ffbc19c5aa7212c08b345ac1ea1a58e31b8a6f3
JaeHyuk Kwack <jkwack@anl.gov>
2020-02-12 20:18:52 -0500

    Update run_after_rebuild.sh for two MPI ranks and command line variables (i.e., INPUT and NQVV)

--- run_after_rebuild.sh
+++ run_after_rebuild.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-if [ "$1" != "" ]; then
-  input=$1
-else
-  input=benz.kern
-fi
 make clean
 make all
-./run_gpu.sh $input $2
-./run_cpu.sh $input $2
+NMPI=2 INPUT=$1 NQVV=$2 ./run_gpu.sh
+NMPI=2 INPUT=$1 NQVV=$2 ./run_cpu.sh
 
 
