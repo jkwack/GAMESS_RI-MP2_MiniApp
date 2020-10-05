33e936d93040b3de25044c3042e50bb6f77f76bc
JaeHyuk Kwack <jkwack@anl.gov>
2020-10-05 12:48:11 -0500

    [CPP]Create Makefile & GAMESS-RIMP2-CorrEng-Tutorial.cpp, and add benz.kern

---
+++ CPP/GAMESS-RIMP2-CorrEng-Tutorial.cpp
@@ -0,0 +1,15 @@
+//  Copyright (C) 2020, Argonne National Laboratory. All Rights Reserved.
+//  Licensed under the NCSA open source license
+
+#include <iostream>
+#include <fstream>
+
+
+
+int main(int argc, char *argv[]){
+
+   std::cout<< "this is a test";
+   return(0);
+
+}
+
