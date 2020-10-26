5e75def0d59895367917ad49ed6541bfa402ebe8
JaeHyuk Kwack <jkwack@anl.gov>
2020-10-26 18:57:36 +0000

    [CPP] V50-MKL-OFFLOAD-OPT02 works only with L0 runtime

--- CPP/RIMP2_Energy_Whole_Combined_V50.cpp
+++ CPP/RIMP2_Energy_Whole_Combined_V50.cpp
@@ -12,69 +12,67 @@ void RIMP2_Energy_Whole_Combined(double *E2){
     double *QVV;
     double E2_local=0.0E0;
     int dnum=0;
-    int nQVV=NVIR*NVIR*NACT;
+    int nQVV=NVIR*NACT*NVIR;
     double *A_tmp, *B_tmp;
+    QVV = new double[nQVV];
 
     #pragma omp target enter data map(alloc:QVV[0:nQVV]) device(dnum)
     #pragma omp target enter data map(to:NAUXBASD,NACT,NVIR,B32size) device(dnum)
     #pragma omp target enter data map(to:eij[0:NACT*NACT],eab[0:NVIR*NVIR],B32[0:B32size]) device(dnum)
-    {
-       QVV = new double[NVIR*NVIR];
-       E2_local = 0.0E0;
+    for(int JACT=0;JACT<NACT;JACT++){
 
-       #pragma omp for schedule(dynamic) 
-       for(int JACT=0;JACT<NACT;JACT++){
-
-           // Compute QVV
-           int m=NVIR*(JACT+1);
-           int n=NVIR;
-           int k=NAUXBASD;
-           int lda=NAUXBASD;
-           int ldb=NAUXBASD;
-           int ldc=NVIR*(JACT+1);
-           double alpha = 1.0;
-           double beta = 0.0;
-           A_tmp = B32;
-           B_tmp = &B32[JACT*NAUXBASD*NVIR];
-           #pragma omp target data map(to:A_tmp[0:m*lda],B_tmp[0:n*ldb])
-           #pragma omp target variant dispatch use_device_ptr(A_tmp,B_tmp,B32,QVV) device(dnum)
-           dgemm("T", "N",
+        // Compute QVV
+        int m=NVIR*(JACT+1);
+        int n=NVIR;
+        int k=NAUXBASD;
+        int lda=NAUXBASD;
+        int ldb=NAUXBASD;
+        int ldc=NVIR*(JACT+1);
+        double alpha = 1.0E0;
+        double beta = 0.0E0;
+        A_tmp = B32;
+        B_tmp = &B32[JACT*NAUXBASD*NVIR];
+        #pragma omp target data map(to:A_tmp[0:m*lda],B_tmp[0:n*ldb])
+        {
+        #pragma omp target variant dispatch use_device_ptr(A_tmp,B_tmp,B32,QVV) device(dnum)
+        dgemm("T", "N",
                 &m,     &n,     &k,
-                &alpha, A_tmp,  &lda,
+                &alpha, B32,  &lda,
                         B_tmp,  &ldb,
+//                        &B32[JACT*NAUXBASD*NVIR],  &ldb,
                 &beta,  QVV,    &ldc);
+        }
 
-           // Accumulate E2
-           #pragma omp target map(tofrom:E2_local) device(dnum)
+        // Accumulate E2
+        #pragma omp target map(tofrom:E2_local) device(dnum)
+        {
+           #pragma omp teams distribute reduction(+:E2_local)
            {
-              #pragma omp teams distribute reduction(+:E2_local)
-              {
-                 for(int IC=0; IC<=JACT; IC++){
-                    double E2_t=0.0;
-                    #pragma omp parallel for reduction(+:E2_t) collapse(2)
-                    {
-                       for(int IB=0; IB<NVIR; IB++){
-                       for(int IA=0; IA<NVIR; IA++){
-                          double Tijab =
-                              QVV[IA+ IC*NVIR+ IB*NVIR*JACT]
-                              / ( eij[IC + JACT*NACT]
-                                  - eab[IA + IB*NVIR] );
-                          double Q_t =
-                              QVV[IA+ IC*NVIR+ IB*NVIR*JACT]
-                              + QVV[IA+ IC*NVIR+ IB*NVIR*JACT];
-                          E2_t += Tijab * (Q_t - QVV[IB+ IC*NVIR+ IA*NVIR*JACT]);
-                       }}   // loop for IA and IB
-                    }   // omp parallel for
-                    double FAC = (IC==JACT) ? (1.0E0) : (2.0E0);
-                    E2_local +=  FAC*E2_t;
-                 }   // loop for IC
-              }   // omp teams distribute
-           }   // omp target 
-       }   // loop for JACT
+              for(int IC=0; IC<(JACT+1); IC++){
+                 double E2_t=0.0;
+                 #pragma omp parallel for reduction(+:E2_t) collapse(2)
+                 {
+                    for(int IB=0; IB<NVIR; IB++){
+                    for(int IA=0; IA<NVIR; IA++){
+                       double Tijab =
+                           QVV[IA+ IC*NVIR+ IB*NVIR*(JACT+1)]
+                           / ( eij[IC + JACT*NACT]
+                               - eab[IA + IB*NVIR] );
+                       double Q_t =
+                           QVV[IA+ IC*NVIR+ IB*NVIR*(JACT+1)]
+                           + QVV[IA+ IC*NVIR+ IB*NVIR*(JACT+1)];
+                       E2_t += Tijab * (Q_t - QVV[IB+ IC*NVIR+ IA*NVIR*(JACT+1)]);
+                    }}   // loop for IA and IB
+                 }   // omp parallel for
+                 double FAC = (IC==JACT) ? (1.0E0) : (2.0E0);
+                 E2_local +=  FAC*E2_t;
+              }   // loop for IC
+           }   // omp teams distribute
+        }   // omp target 
+    }   // loop for JACT
 
-       *E2 = *E2 + E2_local;
-       delete[] QVV;
-    }
+    *E2 = *E2 + E2_local;
+    delete[] QVV;
 
     #pragma omp target exit data map(release:QVV[0:nQVV]) device(dnum)
     #pragma omp target exit data map(release:eij[0:NACT*NACT],eab[0:NVIR*NVIR],B32[0:B32size]) device(dnum)
