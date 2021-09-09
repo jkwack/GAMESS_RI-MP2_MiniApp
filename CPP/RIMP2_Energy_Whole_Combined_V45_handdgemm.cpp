514ca869723b234b33665256f325dee989ae1a70
Colleen Bertoni <bertoni@anl.gov>
2021-09-09 10:19:19 +0000

    adding in hand-dgemm version

--- CPP/RIMP2_Energy_Whole_Combined_V45_handdgemm.cpp
+++ CPP/RIMP2_Energy_Whole_Combined_V45_handdgemm.cpp
@@ -1,10 +1,14 @@
 //  Copyright (C) 2020, Argonne National Laboratory. All Rights Reserved.
 //  Licensed under the NCSA open source license
 
+#ifdef MKL
 #include "mkl.h"
 #include "mkl_omp_offload.h"
+#endif
 #include "common.h"
 #define QVV(I,J,K) QVV[I*NVIR*(JACT+1)+J*NVIR+K]
+
+void dgemm_hand( int m, int n, int k, double *A, double *B, double *C);
 
 void RIMP2_Energy_Whole_Combined(double *E2){
 
@@ -14,23 +18,36 @@ void RIMP2_Energy_Whole_Combined(double *E2){
     QVV = new double[NVIR*NACT*NVIR];
     double *B32J;
     double opm_time_final = 0;
+	
     #pragma omp target enter data map(alloc:QVV[0:NVIR*NACT*NVIR]) device(dnum)
     #pragma omp target enter data map(to:eij[0:NACT*NACT],eab[0:NVIR*NVIR],B32[0:B32size]) device(dnum)
     for(int JACT=0;JACT<NACT;JACT++){
 
         // Compute QVV
-        const long long  m=NVIR*(JACT+1);
-        const long long  n=NVIR;
-        const long long  k=NAUXBASD;
+        int m=NVIR*(JACT+1);
+        int n=NVIR;
+        int k=NAUXBASD;
         double one = 1.0;
         double zero = 0.0;
-        B32J = &B32(JACT,0,0);
-        #pragma omp target variant dispatch use_device_ptr(B32,B32J,QVV) device(dnum)
-        //cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m,n,k,one,B32,k,B32J,k,zero,QVV,m);
-	dgemm("T","N",&m,&n,&k,&one,B32,&k,B32J,&k,&zero,QVV,&m);
-	double opm_time_init=omp_get_wtime();
+
+	B32J = &B32(JACT,0,0);
+
+	printf( "%d out of %d\n", JACT, NACT);
+	dgemm_hand( m,n,k, B32, B32J, QVV);
+	
+	// int cublas_error = cublasDgemm(handle,CUBLAS_OP_T, CUBLAS_OP_N, m,n,k, &one, B32, k, B32J, k, &zero, QVV, m);
+	// if( cublas_error != CUBLAS_STATUS_SUCCESS )
+	//   {
+	//     printf( "failed %d.\n", cublas_error );
+	//     exit(1);
+	//   }
+	// cudaDeviceSynchronize();
+
+	//	double opm_time_init=omp_get_wtime();
+#pragma omp target update to(QVV[0:NVIR*NACT*NVIR])
         // Accumulate E2
-        #pragma omp target teams distribute reduction(+:E2_local) device(dnum)
+	double opm_time_init=omp_get_wtime();
+#pragma omp target teams distribute reduction(+:E2_local)
         for(int IACT=0; IACT<=JACT; IACT++){
            double E2_t=0.0;
            #pragma omp parallel for reduction(+:E2_t)
@@ -43,9 +60,11 @@ void RIMP2_Energy_Whole_Combined(double *E2){
            double FAC = (IACT==JACT) ? (1.0E0) : (2.0E0);
            E2_local +=  FAC*E2_t;
         }   // loop for IACT
-		opm_time_final=opm_time_final+(omp_get_wtime() - opm_time_init);
+	opm_time_final=opm_time_final+(omp_get_wtime() - opm_time_init);
     }   // loop for JACT
+
     printf( "time: %lf\n", opm_time_final);
+    
     *E2 = *E2 + E2_local;
     delete[] QVV;
 
@@ -54,3 +73,21 @@ void RIMP2_Energy_Whole_Combined(double *E2){
 
 }
 
+void dgemm_hand( int m, int n, int k, double * __restrict__ A, double * __restrict__ B , double * __restrict__ C)
+{
+
+  #pragma omp parallel for
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      //      C[i][j] = 0;
+      //      C[i*n+j] = 0;
+      C[j*m+i] = 0;
+      for (int l = 0; l < k; ++l) {
+	//	C[i*n +j] += A[i*k+l]*B[j*k+l];
+	C[j*m +i] += A[i*k+l]*B[j*k+l];
+	//	C[i][j] += A[i][l]*B[l][j];
+	//	C[i][j] += A[i][l]*B[j][l];
+
+      }}}
+  
+}
